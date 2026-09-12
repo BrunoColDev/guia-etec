2e61a60c2895fde38c1342f8212597c398e91d84
BrunoDev <bruno.colangelo2007@gmail.com>
2026-09-11 22:31:07 -0300

    Adiciona hash de senha no cadastro

--- backend/register.php
+++ backend/register.php
@@ -1,23 +1,23 @@
 <?php
 session_start();
-require_once('conexao.php'); // Inclua a conexão com o banco
+require_once('conexao.php'); 
 
-// Habilitar exibição de erros
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$mensagem = '';  // Inicializa a variável de mensagem
-$tipo_mensagem = '';  // Inicializa a variável de tipo de mensagem
+$mensagem = '';  
+$tipo_mensagem = '';  
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Recebe os dados do formulário
+    
     $email = trim($_POST['email']);
     $usuario = trim($_POST['usuario']);
-    $senha = trim($_POST['senha']);  // Captura a senha enviada no formulário
+    $senha = trim($_POST['senha']);  
 
-    // Validações do formulário
+    
     if (empty($email) || empty($usuario) || empty($senha)) {
         $mensagem = 'Todos os campos são obrigatórios.';
         $tipo_mensagem = 'error';
@@ -29,17 +29,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $tipo_mensagem = 'error';
     } else {
         try {
-            // Verifica se o e-mail já está cadastrado
+            
             $stmt = $pdo->prepare("SELECT id FROM usuarios WHERE email = ?");
             $stmt->execute([$email]);
             if ($stmt->fetch()) {
                 $mensagem = 'E-mail já cadastrado.';
                 $tipo_mensagem = 'error';
             } else {
-                // Salva o novo usuário no banco de dados com a senha em texto simples
+                
+                $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+                
                 $stmt = $pdo->prepare("INSERT INTO usuarios (email, usuario, senha) VALUES (?, ?, ?)");
-                $stmt->execute([$email, $usuario, $senha]);  // Usa a senha diretamente
-                  // Redireciona imediatamente para o login
+                $stmt->execute([$email, $usuario, $senha_hash]); 
+                  
             header("Location: ../backend/login.php");
             exit();
 
@@ -47,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $tipo_mensagem = 'success';
             }
         } catch (PDOException $e) {
-            // Se houver erro na consulta, mostre a mensagem de erro
+            
             $mensagem = 'Erro ao cadastrar usuário: ' . $e->getMessage();
             $tipo_mensagem = 'error';
         }
@@ -55,7 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
-<!-- Exibindo as mensagens de erro ou sucesso no front-end -->
+
 <?php if ($mensagem): ?>
     <div class="alert alert-<?php echo $tipo_mensagem; ?>">
         <?php echo $mensagem; ?>
