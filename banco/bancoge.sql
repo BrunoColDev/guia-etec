f71cf8632ab6dd1218966441c646871fcb59ec91
BrunoDev <bruno.colangelo2007@gmail.com>
2026-09-11 22:34:08 -0300

    Aumenta tamanho do campo de senha

--- banco/bancoge.sql
+++ banco/bancoge.sql
@@ -5,7 +5,7 @@ CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) NOT NULL,
     usuario VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL
+    senha VARCHAR(255) NOT NULL
 );
 
 
